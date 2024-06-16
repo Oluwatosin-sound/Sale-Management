71302e06b6dcc3ec20c0b20d7843036ca75e0181
unknown <oluwatosinsulaiman1@gmail.com>
2024-06-16 15:32:43 +0100

    first commit

---
+++ CodeBlock/Sales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlock
+{
+    public class Sales
+    {
+        public int SaleId { get; set; }
+        public int SalesRepId { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public decimal TotalAmountOfSales { get; set; }
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+        public List<SalesItem> ListOfSalesItems { get; set; }
+    }
+}
---
+++ CodeBlock/SalesItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlock
+{
+    public class SalesItem
+    {
+        public Sales SalesSale { get; set; }
+        public int SalesId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal TotalAmountOfProductQuantity { get; set; }
+    }
+}
